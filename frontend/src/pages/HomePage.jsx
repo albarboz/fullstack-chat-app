c17c7304aee3907f612b507b264f941a63f30470
Al <115059113+albarboz@users.noreply.github.com>
2025-03-11 16:28:03 -0700

    Your commit message

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   const { selectedUser } = useChatStore()
 
   return (
-    <div className='min-h-screen bg-base-200'>
+    <div className='min-h-screen bg-base-200 p-2 sm:p-0'>
       <div className='min-h-screen flex items-center justify-center pt-15 px-4'>
         <div className='bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]'>
           <div className='flex h-full rounded-lg overflow-hidden'>
