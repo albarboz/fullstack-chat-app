cea510807e35c9fc6f55f47618e9eca65c81b3f3
Al <115059113+albarboz@users.noreply.github.com>
2025-03-15 19:38:30 -0700

    Your commit message

--- frontend/src/components/skeletons/SidebarSkeleton.jsx
+++ frontend/src/components/skeletons/SidebarSkeleton.jsx
@@ -7,9 +7,9 @@ export const SidebarSkeleton = () => {
         <aside className='min-h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200'>
             {/* Header */}
 
-            <div className='border-b border-base-300 w-full p-5'>
-                <div className='flex items-center gap-2'>
-                    <Users className='w-6 h-6' />
+            <div className='border-b border-base-300 w-full p-'>
+                <div className='flex items-center gap-3'>
+                    <Users className='size-6' />
                     <span className='font-medium hidden lg:block'>Contacts</span>
                 </div>
             </div>
