d89b5e9732522708be57826d514bb2c4d6341f29
Al <115059113+albarboz@users.noreply.github.com>
2025-03-16 20:51:32 -0700

    Your commit message

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
   return (
     <div className='h-dvh bg-base-200'>
       <div className='flex items-center justify-center'>
-        <div className='bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]'>
+        <div className='bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-9rem)]'>
         <div className='flex h-full rounded-lg overflow-hidden'>
             <Sidebar />
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
